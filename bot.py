1de20ef95a1d25e18a076e4d179419f1978d6f38
mia-moran <mariamoranluaces@gmail.com>
2022-01-11 12:57:11 +0100

    token and url added

--- bot.py
+++ bot.py
@@ -1,39 +1,68 @@
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
-import requests
-import re
+"""
+Simple Bot to reply to Telegram messages taken from the python-telegram-bot examples.
+Source: https://github.com/python-telegram-bot/python-telegram-bot/blob/master/examples/echobot2.py
+"""
+
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
 PORT = int(os.environ.get('PORT', 5000))
-TOKEN = os.environ["TOKEN"]
-API_ENDPOINT = 'https://dog.ceo/api/breeds/image/random'
 
-def main():    
-  updater = Updater(TOKEN, use_context=True)    
-  dp = updater.dispatcher  
-  
-  dp.add_handler(CommandHandler('bop', bop))   
-     
-  updater.start_webhook(listen="0.0.0.0",        
-                        port=int(PORT),                       
-                        url_path=TOKEN) 
-  updater.bot.setWebhook('https://your-app-name.herokuapp.com/' + TOKEN) 
-  updater.idle()
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-def bop(update, context):    
-  url = get_image_url()    
-  chat_id = update.message.chat.id       
-  context.bot.send_photo(chat_id=chat_id, photo=url)
+logger = logging.getLogger(__name__)
+TOKEN = '5064593609:AAFCPZe1NZBxo9zuJUacAE8BIraGnhdAhGA'
 
-def get_url():
-    contents = requests.get(API_ENDPOINT).json()
-    url = contents['message']
-    return url
-def get_image_url():
-    allowed_extension = ['jpg', 'jpeg', 'png']
-    file_extension = ''
-    while file_extension not in allowed_extension:
-        url = get_url()
-        file_extension = re.search("([^.]*)$", url).group(1).lower()
-    return url
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://telegram-habits-bot.herokuapp.com/' + TOKEN)
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
 
 if __name__ == '__main__':
     main()
