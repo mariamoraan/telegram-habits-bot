b25b19d4ddf13766587865a0f9166fb9d2477e1c
mia-moran <mariamoranluaces@gmail.com>
2022-01-11 13:36:33 +0100

    .env file postponed

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 load_dotenv()
-TOKEN = os.getenv('TOKEN')
+TOKEN = '5064593609:AAFCPZe1NZBxo9zuJUacAE8BIraGnhdAhGA'
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
